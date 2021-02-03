3f4a89d86f6a0934b90717085b5caa1b25de8835
Siphamandla Sisa Sidelo <67328206+sisasidelo@users.noreply.github.com>
2021-02-03 17:00:15 +0200

    Add files via upload
    Fixed time out after three attempts

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ let spanCscore = document.getElementById("comp-score");
 
 let divScoreBoard = document.querySelector(".scoreboard");
 let divResult = document.querySelector(".result");
+let rsltMsg = document.getElementById("rslt");
 
 let divRock = document.getElementById("rock");
 let divPaper = document.getElementById("paper");
 let divScissor = document.getElementById("scissor");
-
-	function attempt(){
-		attempts++;
-		if (attempts =>3){spanAttempt.innerHTML = attempts;}
-		else {
-			if(uScore > cScore){
-			alert("Game over: You win");
-			setTimeout(function(){reset();},2000);
+	function result (){
+		if(uScore > cScore){
+				alert("Game over: You win");
+				setTimeout(function(){reset();},0);
 			}else if (uScore < cScore){
 				alert("Game Over: You lose!");
+				setTimeout(function(){reset();},0);
 				}
-			else{alert("Draw");
-				setTimeout(function(){reset();},2000);
+			else{
+				alert("Game Over: Draw!");
+				setTimeout(function(){reset();},0);
 				}
+		}
+	function attempt(){
+		attempts++;
+		if (attempts <3){
+			spanAttempt.innerHTML = attempts;}
+		else if(attempts == 3){
+				spanAttempt.innerHTML = attempts;
+				setTimeout(function(){result();},200);
 			}
 		}
 	function reset(){
@@ -58,21 +65,29 @@ let divScissor = document.getElementById("scissor");
 
 	function game(usrChoice){
 		const compChoice = getCompChoice();
+		let rs = "Rock crushes Scissor.";
+		let sp = "Scissor cuts Paper.";
+		let pr = "Paper covers Rock.";
 		switch(usrChoice+compChoice){
 			case 'rs':
 				win();
+				rsltMsg.innerHTML = rs + " You win!";
 				break;
 			case 'sp':
 				win();
+				rsltMsg.innerHTML = sp + " You win!";
 				break;
 			case 'pr':
 				win();
+				rsltMsg.innerHTML = pr + " You win!";
 				break;
 			case 'sr':
 				lose();
+				rsltMsg.innerHTML = rs + "You lose!";
 				break;
 			case 'ps':
 				lose();
+				rsltMsg.innerHTML = sp + "You lose!";
 				break;
 			case 'rp':
 				lose();
@@ -80,7 +95,7 @@ let divScissor = document.getElementById("scissor");
 			case 'rr':
 			case 'pp':
 			case 'ss':
-				draw();
+				rsltMsg.innerHTML = "Draw!";
 				break;
 			}
 		}
